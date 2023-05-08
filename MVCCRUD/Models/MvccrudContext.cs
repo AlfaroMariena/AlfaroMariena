a2eb0cc08603460f3763bed04b5fd47c12e7a233
Cesia <MarienaAlfaro@outlook.es>
2023-05-08 01:13:36 -0500

    Guardando proyecto CRUD

---
+++ MVCCRUD/Models/MvccrudContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<NewTable> NewTables { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+
+    }
+ 
+      
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<NewTable>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("new_table");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Fecha).HasColumnType("datetime");
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Fecha).HasColumnType("datetime");
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVCCRUD/Models/NewTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models;
+
+public partial class NewTable
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int? Edad { get; set; }
+
+    public DateTime? Fecha { get; set; }
+}
